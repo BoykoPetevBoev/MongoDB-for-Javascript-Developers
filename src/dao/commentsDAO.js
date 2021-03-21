2db48d35d50897aa62e07a8f6c13401580f3f81e
BoykoPetevBoev <boykopetevboev@gmail.com>
2021-03-21 16:39:56 +0200

    chapter 3: Admin Backnd

--- src/dao/commentsDAO.js
+++ src/dao/commentsDAO.js
@@ -45,7 +45,7 @@ export default class CommentsDAO {
     try {
       // TODO Ticket: Create/Update Comments
       // Construct the comment document to be inserted into MongoDB.
-      const commentDoc = { 
+      const commentDoc = {
         movie_id: ObjectId(movieId),
         name: user.name,
         email: user.email,
@@ -123,7 +123,22 @@ export default class CommentsDAO {
     try {
       // TODO Ticket: User Report
       // Return the 20 users who have commented the most on MFlix.
-      const pipeline = []
+      const pipeline = [
+        {
+          '$group': {
+            '_id': '$email',
+            'count': {
+              '$sum': 1
+            }
+          }
+        }, {
+          '$sort': {
+            'count': -1
+          }
+        }, {
+          '$limit': 20
+        }
+      ]
 
       // TODO Ticket: User Report
       // Use a more durable Read Concern here to make sure this data is not stale.
